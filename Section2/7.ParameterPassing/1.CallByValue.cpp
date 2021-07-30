679f664abed1c7b3f77937fcc9b21d9366104259
atheeq2003 <atheequrrahaman8@gmail.com>
2021-07-30 16:46:29 +0530

    CallByValue

---
+++ Section2/7.ParameterPassing/1.CallByValue.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int add(int x, int y){
+    int z;
+    z = x + y;
+    return z;
+}
+int main()
+{
+    int a = 24232, b = 3585, c;
+    c = add(a,b);
+    cout << c << endl;
+}
