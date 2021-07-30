5c9eb163157458aef2a2933a9985b75c682a98b5
atheeq2003 <atheequrrahaman8@gmail.com>
2021-07-30 12:57:56 +0530

    DeclarationAndUtilization

---
+++ Section2/6.Functions/DeclarationAndUtilization.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int add(int a, int b){
+    int c;
+    c = a + b;
+    return c;
+}
+int main()
+{
+    int a = 20, b = 30, sum;
+    sum = add(a,b);
+    cout << sum << endl;
+}
