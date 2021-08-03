cedc5d4b24f7af625ef779453f36d940df1f4a57
atheeq2003 <atheequrrahaman8@gmail.com>
2021-08-03 14:06:18 +0530

    CreatingArrayInsideAndRetAdd

---
+++ Section2/8.ArrayAsParameter/2.ArrayInsideFunAndRetAdd.cpp
@@ -0,0 +1,19 @@
+//Creating Array Inside And Returning Address
+#include<bits/stdc++.h>
+using namespace std;
+int * fun(int size){
+    int *p;
+    p = new int[size];
+    for(int i = 0; i < size; i++){
+        p[i] = i + 1;
+    }
+    return p;
+}
+int main()
+{
+    int *ptr, sz = 10;
+    ptr = fun(sz);
+    for (int i = 0; i < sz; i++){
+        cout << ptr[i] << endl;
+    }
+}
