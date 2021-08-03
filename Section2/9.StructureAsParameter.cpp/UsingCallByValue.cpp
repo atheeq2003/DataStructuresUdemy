94f6e71cadfc2ad79b35d035a464b7fd964a21de
atheeq2003 <atheequrrahaman8@gmail.com>
2021-08-03 17:53:06 +0530

    UsingCallByValue

---
+++ Section2/9.StructureAsParameter.cpp/UsingCallByValue.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Rectangle{
+    int length;
+    int breadth;
+};
+void fun(struct Rectangle rect){
+    cout << rect.length << endl << rect.breadth << endl;
+}
+int main()
+{
+    struct Rectangle r = {10, 5};
+    fun(r);
+    cout << r.length << endl << r.breadth << endl;
+
+}
