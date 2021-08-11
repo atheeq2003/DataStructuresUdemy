02a4f509e875d50a811bde63022f351041f27fad
atheeq2003 <atheequrrahaman8@gmail.com>
2021-08-11 18:26:42 +0530

    UsingClass

---
+++ Section2/13.ObjectOrientedProgramming/2.UsingClass.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Rectangle{
+    int length;
+    int breadth;
+public:
+void intialize(int l, int b){
+    length = l;
+    breadth = b;
+}
+int area(){
+    return length * breadth;
+}
+int perimeter(){
+    int p;
+    p = 2 * (length + breadth);
+    return p;
+}
+};
+int main()
+{
+    Rectangle r;
+    int l,b;
+    cin >> l >> b;
+    r.intialize(l, b);
+    int a, peri;
+    a = r.area();
+    peri = r.perimeter();
+    cout << a << endl << peri << endl;
+}
